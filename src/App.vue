5b87f716b33c996da7e646af76fd7de56f58e7c3
Noctua <titgeogeo@gmail.com>
2020-09-24 19:03:18 +0200

    choix type d'horloge

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,62 @@
 <template>
 	<div id="app">
-		<div class="container">
-			<div class="row clock-container">
-				<Clock :day="this.day" :hours="this.hours" :minutes="this.minutes" :seconds="this.seconds"></Clock>
+		<div class="container clock-time">
+			<div class="row">
+				<div class="clock-container col-12">
+					<Clock v-show="!toggleClock" :hours="this.hours" :minutes="this.minutes" :seconds="this.seconds">
+					</Clock>
+					<ClockDigital v-show="toggleClock" :hours="this.hours" :minutes="this.minutes"
+						:seconds="this.seconds"></ClockDigital>
+				</div>
+			</div>
+			<div class="row">
+				<div class="clock_options col-12">
+					<input type="checkbox" id="toggle" class="switch" v-model="toggleClock">
+					<label for="toggle" class="toggle">
+						<span class="toggle_handler"></span>
+					</label>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import Clock from './components/Clock';
+	import Clock from './components/Clock';
+	import ClockDigital from './components/ClockDigital';
 
 	export default {
 		name: 'App',
 		components: {
-			Clock
+			Clock,
+			ClockDigital
 		},
-		data(){
-			return{
-				day : Date,
-				hours: Number,
-				minutes: Number,
-				seconds: Number,
+		data() {
+			return {
+				day: Date,
+				hours: '',
+				minutes: '',
+				seconds: '',
+				toggleClock: false,
 			}
 		},
-		created() {			
-			setInterval(() => {
-				this.day = new Date();
-				this.hours = this.day.getHours() * 30;
-				this.minutes = this.day.getMinutes() * 6;
-				this.seconds = this.day.getSeconds() * 6;
-			},1000);
+
+		methods: {
+			/**
+			 * Récupère le temps avec un interval.
+			 */
+			getTimes() {
+				setInterval(() => {
+					this.day = new Date();
+					this.hours = this.day.getHours();
+					this.minutes = this.day.getMinutes();
+					this.seconds = this.day.getSeconds();
+				});
+			}
+		},
+
+		created() {
+			this.getTimes();
 		}
 	}
 </script>
---
+++ src/components/ClockDigital.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="clock_digital">
+        <div class="clock-col">
+            <p class="hour">{{leadingZero(this.hours)}}</p>
+            <p class="label">Hours</p>
+        </div>
+        <div class="clock-col">
+            <p class="minutes">{{leadingZero(this.minutes)}}</p>
+            <p class="label">Minutes</p>
+        </div>
+        <div class="clock-col">
+            <p class="seconds">{{leadingZero(this.seconds)}}</p>
+            <p class="label">Seconds</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            hours: "",
+            minutes: "",
+            seconds: "",
+        },
+        methods: {
+            /**
+             * On ajoute en premier un zéro si les heures, minutes ou secondes sont inférieur à dix.
+             * @param {number} - Numéro du temps
+             */
+            leadingZero(time) {
+                return (time < 10 ? '0' : '') + time;
+            }
+        },
+    }
+</script>
+
+<style>
+    .clock_digital {
+        margin-bottom: 40px;
+        display: flex;
+        align-items: center;
+        
+        box-shadow: 0 0 0 1px rgba(0, 0, 0, .2);
+    }
+
+    .clock-col {
+        position: relative;
+        text-align: center;
+        min-width: 90px;
+    }
+
+    .clock-col p:first-child {
+        color: #fff;
+        font-size: 5rem;
+        text-shadow: 0 0 20px rgb(10, 175, 230), 0 0 20px rgba(10, 175, 230, 0);
+        font-family: 'monospace';
+        margin: 0px 40px;
+    }
+
+    .clock-col .label {
+        color: #fff;
+        font-size: 1rem;
+        text-transform: uppercase;
+        margin-top: 10px;
+        background-color: #055496;
+        box-shadow: 0 0 0 1px rgba(0, 0, 0, .2);
+        height: 35px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .clock-col:not(:last-child) p::before {
+        content: ':';
+        color: #fff;
+        font-size: 3rem;
+        position: absolute;
+        right: -13px;
+        top: 15%;
+        z-index: 10;
+    }
+
+    @media screen and (max-width: 576px) {
+        .clock-col p:first-child {
+            font-size: 3rem;
+            margin: 0px 20px;
+        }
+
+        .clock-col:not(:last-child) p::before {
+            font-size: 1.5rem;
+            right: -9px;
+        }
+
+        .clock-col .label {
+            font-size: .8rem;
+            height: 25px;
+        }
+    }
+</style>
