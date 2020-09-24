cdea6888f4bcbf54793e526b1f46ebe9220131d8
Noctua <titgeogeo@gmail.com>
2020-09-24 19:06:03 +0200

    update clock & css

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="clock">
-        <div class="hours" :style="{transform: 'rotateZ(' + (this.hours + (this.minutes/12)) + 'deg)'}"></div>
-        <div class="minutes" :style="{transform: 'rotateZ(' + this.minutes + 'deg)'}"></div>
-        <div class="seconds" :style="{transform: 'rotateZ(' + this.seconds + 'deg)'}">
+        <div class="hours" :style="{transform: 'rotateZ(' + degHours + 'deg)'}"></div>
+        <div class="minutes" :style="{transform: 'rotateZ(' + degMinutes + 'deg)'}"></div>
+        <div class="seconds" :style="{transform: 'rotateZ(' + degSeconds + 'deg)'}">
         </div>
         <div class="number" v-for="index in 12" :key="index" :class="'number' + index"></div>
     </div>
@@ -11,31 +11,56 @@
 <script>
     export default {
         props: {
-            day: "",
             hours: "",
             minutes: "",
             seconds: ""
+        },
+
+        computed: {
+            /**
+             * Calcul le degré de rotation de l'aiguille heure.
+             * @returns {number} Degré 
+             */
+            degHours() {
+                return (this.hours * 30) + (this.minutes / 12)
+            },
+
+            /**
+             * Calcul le degré de rotation de l'aiguille minute.
+             * @returns {number} Degré 
+             */
+            degMinutes() {
+                return this.minutes * 6;
+            },
+
+            /**
+             * Calcul le degré de rotation de l'aiguille seconde.
+             * @returns {number} Degré 
+             */
+            degSeconds() {
+                return this.seconds * 6;
+            }
         }
     }
 </script>
 
 <style>
     .clock {
-        height: 350px;
-        width: 350px;
+        height: 300px;
+        width: 300px;
         display: flex;
         justify-content: center;
         align-items: center;
         border-radius: 50%;
-        border: 4px solid rgb(89, 89, 194);
+        border: 4px solid rgba(5, 84, 150,1);
         box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.411),
             inset 5px 5px 5px 0px rgba(0, 0, 0, 0.411),
             5px -5px 5px 0px rgba(0, 0, 0, 0.411),
             inset 5px -5px 5px 0px rgba(0, 0, 0, 0.411);
         color: #fff;
         position: relative;
-        font-family: 'Rubik', sans-serif;
         font-size: 30px;
+        margin-bottom: 40px;
     }
 
     .clock:before {
@@ -51,70 +76,69 @@
     .clock .number {
         --rotation: 0;
         position: absolute;
-        width: 100%;
         height: 100%;
         text-align: center;
         transform: rotate(var(--rotation));
         padding: 10px;
     }
 
-    .number::before{
+    .clock .number::before {
         display: inline-flex;
         transform: rotate(var(--rotation));
         content: '12';
     }
 
-    .number1::before {
+    .clock .number1::before {
         content: '1';
         --rotation: -30deg;
     }
 
-    .number2::before {
+    .clock .number2::before {
         content: '2';
         --rotation: -60deg;
     }
 
-    .number3::before {
+    .clock .number3::before {
         content: '3';
         --rotation: -90deg;
     }
 
-    .number4::before {
+    .clock .number4::before {
         content: '4';
         --rotation: -120deg;
     }
 
-    .number5::before {
+    .clock .number5::before {
         content: '5';
         --rotation: -150deg;
     }
 
-    .number6::before {
+    .clock .number6::before {
         content: '6';
         --rotation: -180deg;
     }
 
-    .number7::before {
+    .clock .number7::before {
         content: '7';
         --rotation: -210deg;
     }
 
-    .number8::before {
+    .clock .number8::before {
         content: '8';
         --rotation: -240deg;
     }
 
-    .number9::before {
+    .clock .number9::before {
         content: '9';
         --rotation: -270deg;
     }
 
-    .number10::before {
+    .clock .number10::before {
         content: '10';
         --rotation: -300deg;
     }
 
-    .number11::before {
+    .clock .number11::before {
         content: '11';
         --rotation: -330deg;
     }
@@ -163,62 +187,58 @@
         --rotation: 330deg
     }
 
-
-
-
-
-
     .clock .hours {
+        width: 130px;
+        height: 130px;
+    }
+
+    .clock .minutes {
         width: 160px;
         height: 160px;
     }
 
-    .clock .minutes {
+    .clock .seconds {
         width: 190px;
         height: 190px;
     }
 
+    .clock .hours,
+    .clock .minutes,
     .clock .seconds {
-        width: 230px;
-        height: 230px;
-    }
-
-    .hours,
-    .minutes,
-    .seconds {
         display: flex;
         justify-content: center;
         position: absolute;
         border-radius: 50%;
     }
 
-    .hours:before {
+    .clock .hours:before {
         content: '';
         position: absolute;
         width: 8px;
-        height: 80px;
+        height: 70px;
         background-color: #ff105e;
         z-index: 10;
         border-radius: 6px 6px 0 0;
     }
 
-    .minutes:before {
+    .clock .minutes:before {
         content: '';
         position: absolute;
         width: 4px;
-        height: 90px;
+        height: 80px;
         background-color: #fff;
         z-index: 11;
         border-radius: 6px 6px 0 0;
     }
 
-    .seconds:before {
+    .clock .seconds:before {
         content: '';
         position: absolute;
         width: 2px;
-        height: 150px;
+        height: 130px;
         background-color: #fff;
         z-index: 12;
         border-radius: 6px 6px 0 0;
     }
+
 </style>
